6e3a2e2e9924df788a394ab9ae000de9e8333258
Fritz Kevin Manurung <89495965+fritzkmanurung@users.noreply.github.com>
2026-05-05 22:05:10 +0700

    Add t00 environment setup to Fundamentals

---
+++ Fundamentals/2122-ge-t00-environment-setup-fritzkmanurung/t00_01.c
@@ -0,0 +1,9 @@
+// 12S21014 - Fritz Kevin Manurung
+#include <stdio.h>
+
+int main(int _argv, char **_argc)
+{
+    printf("Hello, world!");
+
+    return 0;
+}
---
+++ Fundamentals/2122-ge-t00-environment-setup-fritzkmanurung/t00_02.c
@@ -0,0 +1,10 @@
+// 12S21014 - Fritz Kevin Manurung
+
+#include <stdio.h>
+
+int main(int _argv, char **_argc)
+{
+    printf("Aloha!");
+
+    return 0;
+}
