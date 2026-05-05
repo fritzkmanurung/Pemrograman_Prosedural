35cc7c6c11a2c5542af52f776fdf6a77e18ff385
Fritz Kevin Manurung <89495965+fritzkmanurung@users.noreply.github.com>
2026-05-05 22:06:57 +0700

    Add t09 pointer to function to Tasks

---
+++ Tasks/2122-ge-t09-pointer-to-function-fritzkmanurung/libs/dorm.c
@@ -0,0 +1,20 @@
+#include "dorm.h"
+#include <stdio.h>
+#include <string.h>
+
+struct dorm_t create_dorm(char *_name, unsigned short _capacity, enum gender_t _gender) {
+    struct dorm_t dorm;
+    strcpy(dorm.name, _name);
+    dorm.capacity = _capacity;
+    dorm.gender = _gender;
+    dorm.residents_num = 0;
+    return dorm;
+}
+
+void print_dorm(struct dorm_t dorm) {
+    printf("%s|%d|%s\n", dorm.name, dorm.capacity, dorm.gender == GENDER_MALE ? "male" : "female");
+}
+
+void print_dorm_detail(struct dorm_t dorm) {
+    printf("%s|%d|%s|%d\n", dorm.name, dorm.capacity, dorm.gender == GENDER_MALE ? "male" : "female", dorm.residents_num);
+}
